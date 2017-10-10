ef85483e11855bf0bfb3febce5b8af902b13a3ec
NaÁrmin <nagym.armin@gmail.com>
2017-10-10 10:57:30 +0200

    updates code and gitignore

--- arr_mr/main.c
+++ arr_mr/main.c
@@ -3,11 +3,18 @@
 
 int main()
 {
-   //char array[] = "cica";
+   char array1[] = "cica";
     char array[] = {'c', 'i', 'c', 'a', '\0'};
     printf("%s", array);
     scanf("%s", array);
     puts(array);
-    printf("%d", sizeof(array));
+    printf("%d\n", sizeof(array1));
+    printf("%d\n", sizeof(array));
+    int array2[1][1];
+    for (int n = 0; n < 2; n++) {
+        for (int i = 0; i < 7; i++)
+            array2[n][i] = n + i;
+    }
+
     return 0;
 }

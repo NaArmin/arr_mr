80092a729cb0ecd8033394965ebae5860f65579a
NaÁrmin <nagym.armin@gmail.com>
2017-10-10 13:40:31 +0200

    update gitignore again

--- arr_mr/main.c
+++ arr_mr/main.c
@@ -10,10 +10,13 @@ int main()
     puts(array);
     printf("%d\n", sizeof(array1));
     printf("%d\n", sizeof(array));
-    int array2[1][1];
-    for (int n = 0; n < 2; n++) {
-        for (int i = 0; i < 7; i++)
-            array2[n][i] = n + i;
+    int array2[2][7];
+    for (int n = 0; n < 3; n++) {
+        for (int i = 0; i < 7; i++) {
+            array2[n][i] = i + n;
+            printf("%d\t", array2[n][i]);
+        }
+        printf("\n");
     }
 
     return 0;
